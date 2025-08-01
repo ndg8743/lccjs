6a7e3a32bb7738a6eab536c93ddc9a259700175e
ItBeCharlie <charlied713@gmail.com>
2025-08-01 12:49:07 -0400

    feat: Updated layout to use all vertical space

--- src/pages/StackToolPage.jsx
+++ src/pages/StackToolPage.jsx
@@ -407,7 +407,7 @@ function StackToolPage() {
       <div className="flex-1 p-4 overflow-hidden">
         <div className="h-full grid grid-cols-12 grid-rows-6 gap-4">
           {/* Code Editor - Left Side */}
-          <div className="col-span-4 row-span-4 bg-gray-800 rounded-lg shadow-xl overflow-hidden">
+          <div className="col-span-4 row-span-6 bg-gray-800 rounded-lg shadow-xl overflow-hidden">
             <div className="h-full flex flex-col">
               <div className="bg-gray-700 px-4 py-2 text-sm font-semibold">
                 ASSEMBLY CODE
@@ -424,7 +424,7 @@ function StackToolPage() {
             </div>
           </div>
 
-          {/* Output - Bottom Left */}
+          {/* Output - Middle Top */}
           <div className="col-span-4 row-span-1 bg-gray-800 rounded-lg shadow-xl overflow-hidden">
             <div className="h-full flex flex-col">
               <div className="bg-gray-700 px-4 py-2 text-sm font-semibold flex justify-between">
@@ -451,7 +451,7 @@ function StackToolPage() {
             </div>
           </div>
 
-          {/* Controls - Bottom Left */}
+          {/* Controls - Top Right */}
           <div className="col-span-4 row-span-1 bg-gray-800 rounded-lg shadow-xl">
             <div className="h-full flex flex-col">
               <div className="bg-gray-700 px-4 py-2 text-sm font-semibold">
@@ -468,8 +468,8 @@ function StackToolPage() {
             </div>
           </div>
 
-          {/* Stack Visualizer - Top Right */}
-          <div className="col-span-4 row-span-3 bg-gray-800 rounded-lg overflow-hidden shadow-xl">
+          {/* Stack Visualizer - Middle Bottom */}
+          <div className="col-span-4 row-span-5 bg-gray-800 rounded-lg overflow-hidden shadow-xl">
             <div className="h-full flex flex-col">
               <div className="bg-gray-700 px-4 py-2 text-sm font-semibold">
                 STACK VISUALIZATION
@@ -496,7 +496,7 @@ function StackToolPage() {
           </div>
 
           {/* Memory - Bottom Right */}
-          <div className="col-span-4 row-span-1 bg-gray-800 rounded-lg shadow-xl">
+          <div className="col-span-4 row-span-3 bg-gray-800 rounded-lg shadow-xl">
             <MemoryPanel
               memory={memory}
               previousMemory={previousMemory}
